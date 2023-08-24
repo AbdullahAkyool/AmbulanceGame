e74e6941314490a3bfd66106f9afeaebc1450895
Abdullah Akyol <abdullah35akyol@gmail.com>
2023-08-24 11:35:13 +0300

    Assets and Temporary Models Added + First Move Added

---
+++ Assets/Scripts/AmbulanceMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbulanceMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private FloatingJoystick joystick;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    private Vector3 moveVector;
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveVector = Vector3.zero;
+        moveVector.x = joystick.Horizontal * moveSpeed * Time.deltaTime;
+        moveVector.z = joystick.Vertical * moveSpeed * Time.deltaTime;
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            Vector3 direction = Vector3.RotateTowards(transform.forward, moveVector, rotateSpeed * Time.deltaTime, 0f);
+            transform.rotation = Quaternion.LookRotation(direction);
+        }
+
+        rb.MovePosition(rb.position + moveVector);
+    }
+}
